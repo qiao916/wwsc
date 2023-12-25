7ad505a7a4a56fb242341c128d565e01c06d36ac
qiao916 <1152589009@qq.com>
2023-12-25 20:24:23 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+</script>
+<!--一级路由-->
+<template>
+  <header>
+    <el-button type="primary">按钮</el-button>
+<!--   -->
+<!--    <h1>helloworld</h1>-->
+    <RouterView />
+
+  </header>
+
+</template>
+
+<style scoped lang="scss">
+header{
+  color: $priceColor
+}
+</style>
+
+
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+我是首页
+<!--  二级裸游出口-->
+  <router-view/>
+</template>
+
+<style scoped>
+
+</style>
